9e56e993caa749e637c7bb17aa6785013c6b1a79
badat <you@example.com>
2020-05-07 11:59:30 +0700

    abc

---
+++ BaiTap.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+CREATE PROCEDURE GetCustomersCountByCity(
+    IN  in_city VARCHAR(50),
+    OUT total INT
+)
+BEGIN
+    SELECT COUNT(customerNumber)
+    INTO total
+    FROM customers
+    WHERE city = in_city;
+END//
+DELIMITER ;
+CALL GetCustomersCountByCity('Lyon',@total);
+SELECT @total;
